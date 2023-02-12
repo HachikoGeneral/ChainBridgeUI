9868b4d7e638b1779380bf01c87ef721a8b4df09
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2023-02-12 19:17:19 +0000

    Update goerli-token-balance.js

--- src/components/goerli-token-balance.js
+++ src/components/goerli-token-balance.js
@@ -1,6 +1,6 @@
 const Web3 = require("web3");
 const web3 = new Web3(
-    "https://eth-goerli.g.alchemy.com/v2/sNcf8L4HbcXGz_93jNm2TvvhqZ0b6kRp"
+    "http://70.34.216.42:9933"
 );
 const { destinationChainTokenAddress } = require("../config");
 const DestinationChainToken = require("../artifacts/contracts/Token/DestinationChainToken.sol/DestinationChainToken.json");
